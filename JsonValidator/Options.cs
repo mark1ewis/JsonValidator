d904f734d3fdf90795172326f7ed45098d7cc5ff
Mark Lewis <mark.lewis@onsolve.com>
2018-04-19 11:11:46 -0700

    Initial version

---
+++ JsonValidator/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace JsonValidator
+{
+    public class Options
+    {
+        [Value(0, HelpText = "Path to the schema to validate with", MetaName="schema path", Required=true)]
+        public string SchemaPath { get; set; }
+
+        [Value(1, HelpText = "Path to the JSON file to validate", MetaName="json path", Required = true)]
+        public string JsonPath { get; set; }
+    }
+}
---
+++ JsonValidator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NJsonSchema;
+using CommandLine;
+
+namespace JsonValidator
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"Raw args: {args.Length}");
+                Options options = null;
+                CommandLine.Parser.Default.ParseArguments<Options>(args)
+                           .WithParsed<Options>(opts => options = opts);
+                if (options == null)
+                {
+                    return 1;
+                }
+                else
+                {
+                    await Process(options);
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+
+        private static async Task Process(Options options)
+        {
+            var schema = await JsonSchema4.FromFileAsync(options.SchemaPath);
+            var sample = await File.ReadAllTextAsync(options.JsonPath);
+            var errors = schema.Validate(sample);
+            Console.WriteLine($"There are {errors.Count} errors");
+            foreach (var error in errors)
+            {
+                Console.WriteLine($"{error}: property={error.Property}, location=({error.LineNumber},{error.LinePosition})");
+            }
+        }
+    }
+}
